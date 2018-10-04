8ee5249d3e1b4ed9d4635e2aafa2cfcea3077258
Georgiy Bukharov <souljorje@gmail.com>
2018-10-04 13:08:33 +0300

    initial commit

---
+++ components/Test.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1 :class="$style.test">What's wrong?</h1>
+</template>
+
+<style module>
+.test {
+  color: red;
+}
+</style>
+
+<script>
+export default {
+  name: 'test',
+  created() {
+    console.log(this.$style.test);
+  },
+}
+</script>
